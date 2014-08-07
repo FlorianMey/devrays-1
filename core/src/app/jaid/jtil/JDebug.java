3c3c3c893fe19a693ad7ae6ab9fbd7016380ea2e
jaid <stramer@mtd-labs.de>
2014-08-07 14:02:52 +0200

    Refactored and enhanced exception formatting

--- core/src/app/jaid/jtil/JDebug.java
+++ core/src/app/jaid/jtil/JDebug.java
@@ -2,15 +2,26 @@ package app.jaid.jtil;
 
 public class JDebug {
 
-	public static String formatException(Exception exception)
+	private static final int DEFAULT_STACK_ELEMENTS = 5;
+	private static final boolean SHOW_CAUSES = true;
+	private static final boolean SHOW_LINE_NUMBER = true;
+	private static final boolean SHOW_METHOD_NAME = true;
+	private static final String STACK_ELEMENTS_SEPERATOR = " ← ";
+
+	public static String formatException(Throwable exception)
 	{
-		return formatException(exception, 5);
+		return formatException(exception, DEFAULT_STACK_ELEMENTS);
 	}
 
-	public static String formatException(Exception exception, int stackElements)
+	public static String formatException(Throwable exception, int stackElements)
 	{
-		String stackTrace = "";
+		StringBuilder out = new StringBuilder(512);
 		String message = getMessage(exception);
+
+		out.append(exception.getClass().getSimpleName());
+		out.append(message != null && !message.isEmpty() ? "" : ": " + exception.getMessage());
+		out.append(' ');
+
 		StackTraceElement[] stack = exception.getStackTrace();
 
 		int i = 0;
@@ -20,15 +31,33 @@ public class JDebug {
 		while (i != end && stack.length > i)
 		{
 			boolean isSourceHidden = stack[i].getFileName() == null;
-			stackTrace += (first ? "" : " @ ") + (isSourceHidden ? stack[i].getClassName() : stack[i].getFileName().replace(".java", "")) + "." + stack[i].getMethodName() + (isSourceHidden ? "" : ":" + stack[i].getLineNumber());
+			out.append(first ? "" : STACK_ELEMENTS_SEPERATOR);
+
+			out.append(isSourceHidden ? stack[i].getClassName() : stack[i].getFileName().replace(".java", ""));
+
+			if (SHOW_METHOD_NAME && !stack[i].getMethodName().equals("<init>"))
+			{
+				out.append('.');
+				out.append(stack[i].getMethodName());
+			}
+
+			if (SHOW_LINE_NUMBER && !isSourceHidden && stack[i].getLineNumber() > 0)
+				out.append(isSourceHidden ? "" : ":" + stack[i].getLineNumber());
+
 			first = false;
 			i++;
 		}
 
-		return exception.getClass().getSimpleName() + (message != null && !message.isEmpty() ? "" : ": " + exception.getMessage()) + " " + stackTrace;
+		if (stack.length > stackElements)
+			out.append(STACK_ELEMENTS_SEPERATOR + (stack.length - stackElements) + " more");
+
+		if (SHOW_CAUSES && exception.getCause() != null)
+			out.append(" <caused by> " + formatException(exception.getCause(), stackElements));
+
+		return out.toString();
 	}
 
-	public static String getMessage(Exception exception)
+	public static String getMessage(Throwable exception)
 	{
 		return exception.getMessage();
 	}
