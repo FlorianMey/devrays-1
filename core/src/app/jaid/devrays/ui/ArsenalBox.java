3c729ac098b9295ceb2067af2ba7fc9fa0443b98
jaid <stramer@mtd-labs.de>
2014-08-08 11:27:29 +0200

    Added lines of code counting script

---
+++ core/src/app/jaid/devrays/ui/ArsenalBox.java
@@ -0,0 +1,14 @@
+package app.jaid.devrays.ui;
+
+import app.jaid.devrays.mobs.Ship;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+public class ArsenalBox extends Table {
+
+	public ArsenalBox(Ship ship)
+	{
+
+	}
+
+}
